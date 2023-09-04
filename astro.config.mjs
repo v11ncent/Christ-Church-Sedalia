de5a9c82dc5facff21e929f6dd8d02e504bb4a21
Vince <45806875+vince1444@users.noreply.github.com>
2023-09-04 17:55:35 -0400

    Updated site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:3001",
+  site: "https://christchurchsedalia.com",
   experimental: {
     assets: true,
     viewTransitions: true,
